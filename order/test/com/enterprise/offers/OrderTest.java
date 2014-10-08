48d27dd635c5d7ee1e87ee1867da9f8097f376e9
cretzanu <liviugabriel.cretu@gmail.com>
2014-10-08 13:24:23 +0300

    TODO: standalone test for the Order component

--- order/test/com/enterprise/offers/OrderTest.java
+++ order/test/com/enterprise/offers/OrderTest.java
@@ -4,5 +4,15 @@ package com.enterprise.offers;
 public class OrderTest {
 
 //TODO: implement this test!!!
+	//1) configure a persistence unit in persistence.xml
+	//2) create here an EntityManager
+	//3) instantiate  CreateOrderImpl and set the EntityManager
+	//4)create a test Order object
+	//5) begin transaction , execute createOrder method, commit transaction
+	//5.1) createOrder method should return a new version of the Order, with the generated ID attached
+	//7) verify the ID (it means the object is in the DB
+	//8)instantiate ReasdOrderImpl and set the EntityManager
+	//9) execute readOrder() method with the id of the object received in step 6
+	//10) check for the method to return the right Order object
 
 }
